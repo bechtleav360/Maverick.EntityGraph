ba665cc34b5cbfa69dc45d2d116afd3051d3a00b
Sardar <sara.sardar@bechtle.com>
2023-01-02 12:34:53 +0100

    created esco skill graph

---
+++ docs/architecture/graph/SkillGraph/get_skills.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+def get_esco_skills():
+    skills_frame = pd.read_csv('SkillGraph\Data\skills_de.csv')
+    skills = np.array(skills_frame['preferredLabel'])
+    skillType = np.array(skills_frame['skillType'])
+    conceptURI = np.array(skills_frame['conceptUri'])
+    resueLevel = np.array(skills_frame['reuseLevel'])
+    print(f'we have a list of {len(skills)} skills/competences')
+    return skills_frame, skills, skillType, conceptURI, resueLevel
