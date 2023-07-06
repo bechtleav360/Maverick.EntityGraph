c5ecfbcdc24af4be2b2fe737f819e3e72a7b09ac
pajoma <pajoma@gmail.com>
2023-07-06 12:15:21 +0200

    Fix for application type

--- maverick.graph.feature.applications/src/main/java/org/av360/maverick/graph/feature/applications/services/vocab/ApplicationTerms.java
+++ maverick.graph.feature.applications/src/main/java/org/av360/maverick/graph/feature/applications/services/vocab/ApplicationTerms.java
@@ -2,7 +2,6 @@ package org.av360.maverick.graph.feature.applications.services.vocab;
 
 import org.av360.maverick.graph.model.rdf.EntityNamespace;
 import org.av360.maverick.graph.model.rdf.LocalIRI;
-import org.av360.maverick.graph.model.vocabulary.SDO;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Namespace;
 import org.eclipse.rdf4j.model.vocabulary.DC;
@@ -14,7 +13,7 @@ public class ApplicationTerms {
     public static final String NAMESPACE = "https://av360.org/schema#";
     public static final String PREFIX = "avs";
     public static final Namespace NS = EntityNamespace.of(PREFIX, NAMESPACE);
-    public static final IRI TYPE = SDO.DATASET;
+    public static final IRI TYPE = LocalIRI.from(NAMESPACE, "Application");
     public static final IRI CONFIGURATION_ITEM = LocalIRI.from(NAMESPACE, "ConfigurationItem");
     public static final IRI ACTIVE_FEATURE = LocalIRI.from(NAMESPACE, "hasFeature");
     public static final IRI HAS_API_KEY = LocalIRI.from(NAMESPACE, "hasApiKey");
