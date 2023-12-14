d64c7bcfe69c53622eb985181333ec888a158dd3
pajoma <pajoma@gmail.com>
2023-12-14 17:17:37 +0100

    Make sure the storage dir is created

--- maverick.graph.feature.objects-store/src/main/java/org/av360/maverick/graph/feature/objects/services/FileServices.java
+++ maverick.graph.feature.objects-store/src/main/java/org/av360/maverick/graph/feature/objects/services/FileServices.java
@@ -83,11 +83,13 @@ public class FileServices {
                     return sd;
                 })
                 .doOnNext(contentLocation -> log.debug("Writing file with id '{}' into uri '{}'", contentLocation.getIdentifier().getLocalName(), contentLocation.getStoragePath().toUri()))
-                .flatMap(localStorageDetails ->
-
-                        DataBufferUtils.write(bytes, localStorageDetails.getStoragePath(), StandardOpenOption.CREATE)
+                .flatMap(localStorageDetails -> {
+                        localStorageDetails.getStoragePath().toFile().mkdirs();
+                        return DataBufferUtils.write(bytes, localStorageDetails.getStoragePath(), StandardOpenOption.CREATE)
                                 .doOnError(error -> log.error("Failed to store file due to reason: {}", error.getMessage()))
-                                .then(Mono.just(localStorageDetails)))
+                                .then(Mono.just(localStorageDetails));
+                        }
+                )
                 .map(localStorageDetails -> localStorageDetails.setDetails(localStorageDetails.getStoragePath().toFile()))
                 .flatMap(sd -> {
                     ValueFactory vf = SimpleValueFactory.getInstance();
